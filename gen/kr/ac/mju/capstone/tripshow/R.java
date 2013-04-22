bd6df5c7a0669c97e930073b4793daf7623a460a
Kimhyunsoo <kimhyunsoo86@gmail.com>
2013-04-23 01:20:32 +0900

    ..

--- gen/kr/ac/mju/capstone/tripshow/R.java
+++ gen/kr/ac/mju/capstone/tripshow/R.java
@@ -13,36 +13,54 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon_list_btn=0x7f020001;
-        public static final int icon_search_btn=0x7f020002;
-        public static final int list_btn_selector=0x7f020003;
+        public static final int icon_lock=0x7f020002;
+        public static final int icon_search_btn=0x7f020003;
+        public static final int list_btn_selector=0x7f020004;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f070004;
-        public static final int list_btn=0x7f070002;
-        public static final int login_btn=0x7f070001;
-        public static final int menu_settings=0x7f070005;
-        public static final int search_btn=0x7f070003;
-        public static final int signup_btn=0x7f070000;
+        public static final int LinearLayout01=0x7f07000d;
+        public static final int LinearLayout_search=0x7f07000c;
+        public static final int email_edit=0x7f070006;
+        public static final int list_btn=0x7f07000a;
+        public static final int login_btn=0x7f070002;
+        public static final int login_id_edit=0x7f070000;
+        public static final int login_pw_edit=0x7f070001;
+        public static final int menu_settings=0x7f07000e;
+        public static final int pw1_edit=0x7f070007;
+        public static final int pw2_edit=0x7f070008;
+        public static final int search_btn=0x7f07000b;
+        public static final int signup_btn=0x7f070003;
+        public static final int signup_request_btn=0x7f070009;
+        public static final int user_firstName_edit=0x7f070004;
+        public static final int user_lastName_edit=0x7f070005;
     }
     public static final class layout {
         public static final int intro_layout=0x7f030000;
-        public static final int login_or_signup_layout=0x7f030001;
-        public static final int tab1_layout=0x7f030002;
-        public static final int tab2_layout=0x7f030003;
-        public static final int tab3_layout=0x7f030004;
-        public static final int tab4_layout=0x7f030005;
-        public static final int tabs_lyaout=0x7f030006;
+        public static final int login_layout=0x7f030001;
+        public static final int login_or_signup_layout=0x7f030002;
+        public static final int signup_layout=0x7f030003;
+        public static final int tab1_layout=0x7f030004;
+        public static final int tab2_layout=0x7f030005;
+        public static final int tab3_layout=0x7f030006;
+        public static final int tabs_lyaout=0x7f030007;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int email_signup=0x7f04000a;
         public static final int hello_world=0x7f040001;
+        public static final int id_hint=0x7f040006;
         public static final int img_desc=0x7f040003;
         public static final int login_btn=0x7f040005;
         public static final int menu_settings=0x7f040002;
+        public static final int pw1_signup=0x7f04000b;
+        public static final int pw2_signup=0x7f04000c;
+        public static final int pw_hint=0x7f040007;
         public static final int signup_btn=0x7f040004;
+        public static final int user_firstName_signup=0x7f040009;
+        public static final int user_lastName_signup=0x7f040008;
     }
     public static final class style {
         /** 
