4a5e11e81db0443dde66faf328639d88ccf802ab
Kimhyunsoo <kimhyunsoo86@gmail.com>
2013-04-27 14:15:41 +0900

    test

--- src/kr/ac/mju/capstone/tripshow/constants/TSConstants.java
+++ src/kr/ac/mju/capstone/tripshow/constants/TSConstants.java
@@ -1,7 +1,7 @@
 package kr.ac.mju.capstone.tripshow.constants;
 
 public class TSConstants {
-		
 		//crypto
 		public static final String CRYPTO_SEED_KEY ="60dc26ddc7604defb7e83da1eb37dc3f";
+		//
 }
